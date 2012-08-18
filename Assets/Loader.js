e48b5e7d95c33cc61bf90cbf6fcd1f1f3b21594e
Julian Ceipek <julian.ceipek@gmail.com>
2012-08-18 02:43:26 -0400

    Added JSON test.

---
+++ Assets/Loader.js
@@ -0,0 +1,15 @@
+#pragma strict
+
+function Start () {
+	var test : String = "{\"entities\" : [{\"x\":12, \"y\":13, \"z\":13}, {\"x\":67, \"y\":13, \"z\":13}]}";
+	Debug.Log(test);
+	var parsed : Hashtable = JSONParse.JSONParse(test);
+	var entities = parsed["entities"];
+	for (var i : Hashtable in entities) {
+		Debug.Log(i["x"]);
+	}
+}
+
+function Update () {
+
+}
