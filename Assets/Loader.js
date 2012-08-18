7b7d8fe65f048349145c933f6d1b8b29e911485e
Julian Ceipek <julian.ceipek@gmail.com>
2012-08-18 02:45:31 -0400

    Rebuilt Lib; removed a warning.

--- Assets/Loader.js
+++ Assets/Loader.js
@@ -3,7 +3,7 @@
 function Start () {
 	var test : String = "{\"entities\" : [{\"x\":12, \"y\":13, \"z\":13}, {\"x\":67, \"y\":13, \"z\":13}]}";
 	Debug.Log(test);
-	var parsed : Hashtable = JSONParse.JSONParse(test);
+	var parsed = JSONParse.JSONParse(test);
 	var entities = parsed["entities"];
 	for (var i : Hashtable in entities) {
 		Debug.Log(i["x"]);
