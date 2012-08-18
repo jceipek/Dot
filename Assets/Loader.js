ae188ae86401f7e1564faa56cb5797af9222b3d4
Julian Ceipek <julian.ceipek@gmail.com>
2012-08-18 15:46:08 -0400

    Added basic level loading behavior.

--- Assets/Loader.js
+++ Assets/Loader.js
@@ -1,13 +1,37 @@
 #pragma strict
 
+import System.IO;
+import System.Collections.Generic;
+
 function Start () {
-	var test : String = "{\"entities\" : [{\"x\":12, \"y\":13, \"z\":13}, {\"x\":67, \"y\":13, \"z\":13}]}";
-	Debug.Log(test);
-	var parsed = JSONParse.JSONParse(test);
-	var entities = parsed["entities"];
-	for (var i : Hashtable in entities) {
-		Debug.Log(i["x"]);
-	}
+	
+    try {
+        // Create an instance of StreamReader to read from a file.
+        var sr = new StreamReader("demolvl.json");
+
+        // Read and display lines from the file until the end of the file is reached.
+        var lines = sr.ReadToEnd();
+
+		var parsed = JSONParse.JSONParse(lines);
+		var entities = parsed["entities"] as Array;
+		for (var i : int; i < entities.length; i++) {
+			var ent = entities[i] as Hashtable;
+			var type = ent["type"] as String;
+			var pos = ent["pos"] as Array;
+			Instantiate(Resources.Load(type), new Vector3(pos[0], pos[1], pos[2]), Quaternion.identity);
+			Debug.Log("Created Object of type " + type + " at " + pos);
+		}
+		
+        sr.Close();
+    }
+
+    catch (e) {
+        // Let the user know what went wrong.
+        print("The file could not be read:");
+        print(e.Message);
+    }
+    
+
 }
 
 function Update () {
